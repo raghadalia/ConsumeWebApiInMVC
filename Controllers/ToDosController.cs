96436ca8e68fc8ef13e1699cbd875bbfd2cb446a
raghadalia <116754310+raghadalia@users.noreply.github.com>
2024-03-25 10:45:15 +0200

    Update ToDosController.cs

--- Controllers/ToDosController.cs
+++ Controllers/ToDosController.cs
@@ -53,7 +53,6 @@ namespace ToDo.Controllers
 
         // POST: ToDos/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,Description,IsCompleted,DueDate,Categories,PriorityLevel")] ToDos toDos)
@@ -84,8 +83,6 @@ namespace ToDo.Controllers
         }
 
         // POST: ToDos/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,IsCompleted,DueDate,Categories,PriorityLevel")] ToDos toDos)
