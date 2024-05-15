454e97b91eddfa5a63a82a4fe3f3f2acfed72044
raghadalia <116754310+raghadalia@users.noreply.github.com>
2024-05-16 02:29:13 +0300

    Update Register.cshtml.cs

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -88,7 +88,7 @@ namespace ToDo.Areas.Identity.Pages.Account
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
-            [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$", ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit and one special character.")]
+            //[RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$", ErrorMessage = "Password must contain at least one lowercase letter, one uppercase letter, one digit and one special character.")]
             public string Password { get; set; }
             
 
